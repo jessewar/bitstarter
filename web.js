1b21669763eac720c2ecb7fb21d4c70a1180c005
Jesse Warren <jesse.warren@live.com>
2013-08-19 22:41:45 +0000

    Added content page. /content now valid

--- web.js
+++ web.js
@@ -5,9 +5,17 @@ var app = express.createServer(express.logger());
 var buffer = fs.readFileSync("index.html");
 var str = buffer.toString();
 
+var contentBuffer = fs.readFileSync("content-page.html");
+var contentStr = contentBuffer.toString();
+
 app.get('/', function(request, response) {
   response.send(str);
 });
+
+app.get('/content', function(request, response) {
+    response.send(contentStr);
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
