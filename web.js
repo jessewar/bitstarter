c26b9e08504d3000f4e69a62e6df4a74b4b64a57
Jesse Warren <jesse.warren@live.com>
2013-08-27 00:37:01 +0000

    added mongoDB to list of dependencies

--- web.js
+++ web.js
@@ -27,8 +27,6 @@ app.get('/', function(request, response) {
 
 // load content page
 app.get('/content', function(request, response) {
-//    response.send(contentStr);
-
     // Find one document in our collection
     db.collection('farm').findOne({}, function(err, doc) {
 	if(err) throw err;
@@ -40,7 +38,8 @@ app.get('/content', function(request, response) {
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
-
+  
+  // connect to MongoDB server on my EC2 instance
   mongoclient.open(function(err, mongoclient) {
     if(err) throw err;
 
