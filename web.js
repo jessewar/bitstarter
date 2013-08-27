3d9f16722a5b28ead22a3daad2018a3b39e5c6cf
Jesse Warren <jesse.warren@live.com>
2013-08-27 03:57:25 +0000

    tried to connect to remote db, failed

--- web.js
+++ web.js
@@ -5,8 +5,15 @@ var cons = require('consolidate');
 var MongoClient = require('mongodb').MongoClient;
 var Server = require('mongodb').Server;
 
-var mongoclient = new MongoClient(new Server("ip-10-243-46-189", 28017));
+
+
+var textUri = "mongodb://heroku_app17145481:iq6hv1i7kuknq833rcoome4q57@ds041198.mongolab.com:41198/heroku_app17145481";
+// var uri = new MongoClientURI(textUri);
+// var mongoclient = new MongoClient(uri);
+
+var mongoclient = new MongoClient(new Server("10.226.119.215", 41198));
 var db = mongoclient.db('bitstarter');
+
 
 app.engine('html', cons.swig);  // tell express to use swig as the html templating engine
 app.set('views', __dirname);  // tell express to treat files in main directory as views
@@ -27,8 +34,9 @@ app.get('/', function(request, response) {
 
 // load content page
 app.get('/content', function(request, response) {
+
     // Find one document in our collection
-    db.collection('farm').findOne({'name':'MyFarm2'}, function(err, doc) {
+    db.collection('farm').findOne({}, function(err, doc) {
 	if(err) throw err;
 
 	response.render('content-page.html', doc);
@@ -40,8 +48,10 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 
   // connect to MongoDB server on my EC2 instance
-  mongoclient.open(function(err, mongoclient) {
+  mongoclient.open(function(err, client) {
     if(err) throw err;
+
+    client.authenticate("jessewar", "lemonlime1");
 
     console.log('Listening to DB');
   });
