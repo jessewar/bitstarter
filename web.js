3ea324d0cae40141afb44503937a3a5b49a3e948
Jesse Warren <jesse.warren@live.com>
2013-08-27 00:17:56 +0000

    communication with DB successful, content-page rendered with DB data

--- web.js
+++ web.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
+var app = express();
+var cons = require('consolidate');
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+
+var mongoclient = new MongoClient(new Server("ip-10-243-46-189", 27017));
+var db = mongoclient.db('bitstarter');
+
+app.engine('html', cons.swig);  // tell express to use swig as the html templating engine
+app.set('views', __dirname);  // tell express to treat files in main directory as views
 
 var buffer = fs.readFileSync("index.html");
 var str = buffer.toString();
@@ -18,10 +27,23 @@ app.get('/', function(request, response) {
 
 // load content page
 app.get('/content', function(request, response) {
-    response.send(contentStr);
+//    response.send(contentStr);
+
+    // Find one document in our collection
+    db.collection('farm').findOne({}, function(err, doc) {
+	if(err) throw err;
+
+	response.render('content-page.html', doc);
+    });
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
+
+  mongoclient.open(function(err, mongoclient) {
+    if(err) throw err;
+
+    console.log('Listening to DB');
+  });
 });
