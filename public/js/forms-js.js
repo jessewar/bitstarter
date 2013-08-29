1bf459bd72a1b05d2a560f04d413c306cc21adc1
Jesse Warren <jesse.warren@live.com>
2013-08-29 04:29:08 +0000

    trying to connect to db in forms-js.js

--- public/js/forms-js.js
+++ public/js/forms-js.js
@@ -3,36 +3,49 @@ $(document).ready(function() {
 });
 
 function submitClicked() {
-  var farmTypes = getFarmTypes();
-  var farmName = getFarmName();
+  $('.btn').on('click', function() {
+    var farmName = getFarmName();
+    var farmTypes = getFarmTypes();
+
+    putIntoDb(farmName, farmTypes);
+  });
 }
 
 // returns the name of the farm as a string
 function getFarmName() {
-  $('.btn').on('click', function() {
-    var farmName = $('#farm-name-textbox').val();
-    $('#test2').text(farmName);
+  var farmName = $('#farm-name-textbox').val();
+  $('#test2').text(farmName);
 
-    return farmName;
-  });
+  return farmName;
 }
 
 // returns an array containing the types of goods a farm has available
 function getFarmTypes() {
-  $('.btn').on('click', function() {
-    var arr = $('#checkboxes').serializeArray(); // array of objects that were checked
+  var arr = $('#checkboxes :checked'); // array of checkboxes that were checked
 
-    var farmTypes = [];
-    for (var i = 0; i < arr.length; i++) {
-      farmTypes[i] = arr[i].name;  // get the types of farm products into an array
-    }
-    
-    $('#test').text(farmTypes.toString());
+  var farmTypes = [];
+  for (var i = 0; i < arr.length; i++) {
+    farmTypes[i] = arr[i].name;
+  }
 
-    return farmTypes;
-  });  
+  $('#test').text(farmTypes.toString());
+  return farmTypes;
 }
 
 function putIntoDb(farmName, farmTypes) {
-  
+  console.log('got here1 ');
+  var server = new Server("ds041198.mongolab.com", 41198); // ip address 10.226.119.215
+  console.log('got here');
+  var db = new Db('heroku_app17145481', server);
+
+  db.open(function(err, client) {
+    if(err) throw err;
+    console.log("connected again!");
+
+  client.authenticate("jessewar", "lemonlime1", function(err, success) {
+    var docs = db.farm.find();
+    $('#test2').text(JSON.stringify(docs));
+  });
+
+  });
 }
