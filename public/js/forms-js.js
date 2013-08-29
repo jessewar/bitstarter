930a18e18a5f7f2d198f2eafcf813129d1589703
Jesse Warren <jesse.warren@live.com>
2013-08-29 03:11:15 +0000

    took out test directory, able to obtain checkbox and text form data

--- public/js/forms-js.js
+++ public/js/forms-js.js
@@ -2,9 +2,37 @@ $(document).ready(function() {
   submitClicked();
 });
 
-var submitClicked = function() {
+function submitClicked() {
+  var farmTypes = getFarmTypes();
+  var farmName = getFarmName();
+}
+
+// returns the name of the farm as a string
+function getFarmName() {
   $('.btn').on('click', function() {
-    var str = $('#checkboxes').serializeArray();
-    $('#test').text(JSON.stringify(str));        
+    var farmName = $('#farm-name-textbox').val();
+    $('#test2').text(farmName);
+
+    return farmName;
   });
-};
+}
+
+// returns an array containing the types of goods a farm has available
+function getFarmTypes() {
+  $('.btn').on('click', function() {
+    var arr = $('#checkboxes').serializeArray(); // array of objects that were checked
+
+    var farmTypes = [];
+    for (var i = 0; i < arr.length; i++) {
+      farmTypes[i] = arr[i].name;  // get the types of farm products into an array
+    }
+    
+    $('#test').text(farmTypes.toString());
+
+    return farmTypes;
+  });  
+}
+
+function putIntoDb(farmName, farmTypes) {
+  
+}
