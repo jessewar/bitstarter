8442b716445493efce558ed598cf05845dddb768
Jesse Warren <jesse.warren@live.com>
2013-08-30 02:33:30 +0000

    added a views directory for html pages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var cons = require('consolidate');
 
 function start(app) {
   app.engine('html', cons.swig);  // tell express to use swig as the html templating engine
-  app.set('views', __dirname);  // tell express to treat files in main directory as views
+  app.set('views', __dirname + '/views');  // tell express to treat files in main directory as views
 
   // load anything in the /public directory as static content on the server
   app.use(express.static(__dirname + '/public'));
